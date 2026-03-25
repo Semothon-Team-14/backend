0ed5e6369c8e4eb0808edf495d1090f87ae85180
ystc1247 <ystc1247@khu.ac.kr>
2026-03-25 19:14:28 +0900

    Add sex to user dto

--- src/main/kotlin/semo/backend/dto/UserDto.kt
+++ src/main/kotlin/semo/backend/dto/UserDto.kt
@@ -1,11 +1,14 @@
 package semo.backend.dto
 
+import semo.backend.enums.Sex
+
 data class UserDto(
     val id: Long,
     val username: String?,
     val name: String?,
     val email: String?,
     val phone: String?,
+    val sex: Sex?,
     val introduction: String?,
     val nationalityId: Long?,
     val keywords: List<KeywordDto>,
