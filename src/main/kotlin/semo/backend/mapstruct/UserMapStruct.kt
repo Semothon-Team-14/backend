5b5fedc8af1c5b6b2405c220f84cec509fac1ccb
ystc1247 <ystc1247@khu.ac.kr>
2026-03-25 14:32:56 +0900

    refactor: remove mapstruct patch update mapping

--- src/main/kotlin/semo/backend/mapstruct/UserMapStruct.kt
+++ src/main/kotlin/semo/backend/mapstruct/UserMapStruct.kt
@@ -1,13 +1,9 @@
 package semo.backend.mapstruct
 
-import org.mapstruct.BeanMapping
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
 import org.mapstruct.MappingConstants
-import org.mapstruct.MappingTarget
-import org.mapstruct.NullValuePropertyMappingStrategy
 import semo.backend.controller.request.CreateUserRequest
-import semo.backend.controller.request.UpdateUserRequest
 import semo.backend.dto.UserDto
 import semo.backend.entity.User
 
@@ -21,15 +17,4 @@ interface UserMapStruct : GenericMapStruct<User, UserDto> {
     @Mapping(target = "updatedBy", ignore = true)
     @Mapping(target = "updatedDateTime", ignore = true)
     fun toEntity(request: CreateUserRequest): User
-
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "createdBy", ignore = true)
-    @Mapping(target = "createdDateTime", ignore = true)
-    @Mapping(target = "updatedBy", ignore = true)
-    @Mapping(target = "updatedDateTime", ignore = true)
-    fun updateEntityFromRequest(
-        request: UpdateUserRequest,
-        @MappingTarget entity: User,
-    )
 }
