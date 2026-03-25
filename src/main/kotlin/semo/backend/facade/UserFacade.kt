8fb59eb7b949d75fa29087ab21cae5b006d52f18
ystc1247 <ystc1247@khu.ac.kr>
2026-03-25 14:24:28 +0900

    feat: add user facade crud methods

--- src/main/kotlin/semo/backend/facade/UserFacade.kt
+++ src/main/kotlin/semo/backend/facade/UserFacade.kt
@@ -1,13 +1,32 @@
 package semo.backend.facade
 
 import org.springframework.stereotype.Service
+import semo.backend.controller.request.CreateUserRequest
+import semo.backend.controller.request.UpdateUserRequest
 import semo.backend.dto.UserDto
+import semo.backend.service.UserService
 
 @Service
 class UserFacade(
     private val userService: UserService,
 ) {
-    fun getUsers(): List<UserDto>{
+    fun getUsers(): List<UserDto> {
         return userService.getUsers()
+    }
+
+    fun getUser(userId: Long): UserDto {
+        return userService.getUser(userId)
+    }
+
+    fun createUser(request: CreateUserRequest): UserDto {
+        return userService.createUser(request)
+    }
+
+    fun updateUser(userId: Long, request: UpdateUserRequest): UserDto {
+        return userService.updateUser(userId, request)
+    }
+
+    fun deleteUser(userId: Long): Long {
+        return userService.deleteUser(userId)
     }
 }
