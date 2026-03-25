9166e800f92c4803a8fe1290d317968bc3e25ae0
ystc1247 <ystc1247@khu.ac.kr>
2026-03-25 19:14:28 +0900

    Add sex to create user request

--- src/main/kotlin/semo/backend/controller/request/CreateUserRequest.kt
+++ src/main/kotlin/semo/backend/controller/request/CreateUserRequest.kt
@@ -1,11 +1,14 @@
 package semo.backend.controller.request
 
+import semo.backend.enums.Sex
+
 data class CreateUserRequest(
     val username: String,
     val password: String,
     val name: String,
     val email: String,
     val phone: String,
+    val sex: Sex? = null,
     val introduction: String? = null,
     val nationalityId: Long? = null,
     val keywordIds: List<Long> = emptyList(),
