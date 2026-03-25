83ddae1ff3fad6fde26ab8df340a219ddb730bfc
ystc1247 <ystc1247@khu.ac.kr>
2026-03-25 14:24:28 +0900

    refactor: simplify user repository declaration

--- src/main/kotlin/semo/backend/repository/jpa/UserRepository.kt
+++ src/main/kotlin/semo/backend/repository/jpa/UserRepository.kt
@@ -2,9 +2,7 @@ package semo.backend.repository.jpa
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import semo.backend.dto.UserDto
 import semo.backend.entity.User
 
 @Repository
-interface UserRepository: JpaRepository<User, Long>{
-}
+interface UserRepository : JpaRepository<User, Long>
