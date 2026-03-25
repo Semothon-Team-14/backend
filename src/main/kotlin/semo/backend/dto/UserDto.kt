ecb6e51d819491aea9a03a81cc73e9e3fa39cda9
ystc1247 <ystc1247@khu.ac.kr>
2026-03-25 14:32:56 +0900

    refactor: allow nullable user dto fields

--- src/main/kotlin/semo/backend/dto/UserDto.kt
+++ src/main/kotlin/semo/backend/dto/UserDto.kt
@@ -2,8 +2,8 @@ package semo.backend.dto
 
 data class UserDto(
     val id: Long,
-    val username: String,
-    val name: String,
-    val email: String,
-    val phone: String,
+    val username: String?,
+    val name: String?,
+    val email: String?,
+    val phone: String?,
 )
