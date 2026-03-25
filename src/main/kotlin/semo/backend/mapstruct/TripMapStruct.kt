1ab58763ea73e468890b71fecb452508f046dc4a
ystc1247 <ystc1247@khu.ac.kr>
2026-03-25 14:56:16 +0900

    feat: add trip mapstruct mappings

---
+++ src/main/kotlin/semo/backend/mapstruct/TripMapStruct.kt
@@ -0,0 +1,24 @@
+package semo.backend.mapstruct
+
+import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+import org.mapstruct.MappingConstants
+import semo.backend.controller.request.CreateTripRequest
+import semo.backend.dto.TripDto
+import semo.backend.entity.Trip
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+interface TripMapStruct : GenericMapStruct<Trip, TripDto> {
+    @Mapping(target = "userId", source = "user.id")
+    @Mapping(target = "cityId", source = "city.id")
+    override fun toDto(entity: Trip): TripDto
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "createdDateTime", ignore = true)
+    @Mapping(target = "updatedBy", ignore = true)
+    @Mapping(target = "updatedDateTime", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "city", ignore = true)
+    fun toEntity(request: CreateTripRequest): Trip
+}
