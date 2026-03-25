63389eacafc4563186ca55e4a6a3fb37195d7da8
ystc1247 <ystc1247@khu.ac.kr>
2026-03-25 17:19:00 +0900

    feat: add city update request

---
+++ src/main/kotlin/semo/backend/controller/request/UpdateCityRequest.kt
@@ -0,0 +1,8 @@
+package semo.backend.controller.request
+
+import java.util.Optional
+
+data class UpdateCityRequest(
+    val cityNameEnglish: Optional<String>? = null,
+    val cityNameKorean: Optional<String>? = null,
+)
