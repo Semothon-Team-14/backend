9ccf7158ebfe0186ab9fd81423184be0650b2c5f
ystc1247 <ystc1247@khu.ac.kr>
2026-03-25 15:56:32 +0900

    refactor: use patch value in trip update request

--- src/main/kotlin/semo/backend/controller/request/UpdateTripRequest.kt
+++ src/main/kotlin/semo/backend/controller/request/UpdateTripRequest.kt
@@ -1,12 +1,11 @@
 package semo.backend.controller.request
 
-import org.openapitools.jackson.nullable.JsonNullable
 import java.time.LocalDate
 
 data class UpdateTripRequest(
-    val title: JsonNullable<String?> = JsonNullable.undefined(),
-    val startDate: JsonNullable<LocalDate?> = JsonNullable.undefined(),
-    val endDate: JsonNullable<LocalDate?> = JsonNullable.undefined(),
-    val userId: JsonNullable<Long?> = JsonNullable.undefined(),
-    val cityId: JsonNullable<Long?> = JsonNullable.undefined(),
+    val title: PatchValue<String?> = PatchValue.undefined(),
+    val startDate: PatchValue<LocalDate?> = PatchValue.undefined(),
+    val endDate: PatchValue<LocalDate?> = PatchValue.undefined(),
+    val userId: PatchValue<Long?> = PatchValue.undefined(),
+    val cityId: PatchValue<Long?> = PatchValue.undefined(),
 )
