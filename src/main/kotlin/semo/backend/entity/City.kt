528c1c50d23d571a9bfd65da4580b7a6f4cd876e
ystc1247 <ystc1247@khu.ac.kr>
2026-03-25 14:36:50 +0900

    feat: add city entity

---
+++ src/main/kotlin/semo/backend/entity/City.kt
@@ -0,0 +1,29 @@
+package semo.backend.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "cities")
+class City(
+    @Column(name = "city_name_english", nullable = false)
+    var cityNameEnglish: String,
+
+    @Column(name = "city_name_korean", nullable = false)
+    var cityNameKorean: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "nationality_id", nullable = false)
+    var nationality: Nationality,
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0
+}
