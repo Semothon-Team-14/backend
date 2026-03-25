6b140454384436e106445ade759908e6729b687d
ystc1247 <ystc1247@khu.ac.kr>
2026-03-25 15:10:04 +0900

    refactor: map user dto profile relations explicitly

--- src/main/kotlin/semo/backend/mapstruct/UserMapStruct.kt
+++ src/main/kotlin/semo/backend/mapstruct/UserMapStruct.kt
@@ -3,13 +3,32 @@ package semo.backend.mapstruct
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
 import org.mapstruct.MappingConstants
+import org.springframework.beans.factory.annotation.Autowired
 import semo.backend.controller.request.CreateUserRequest
 import semo.backend.dto.UserDto
+import semo.backend.entity.Keyword
 import semo.backend.entity.User
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
-interface UserMapStruct : GenericMapStruct<User, UserDto> {
-    override fun toDto(entity: User): UserDto
+abstract class UserMapStruct : GenericMapStruct<User, UserDto> {
+    @Autowired
+    protected lateinit var keywordMapStruct: KeywordMapStruct
+
+    override fun toDto(entity: User): UserDto {
+        return UserDto(
+            id = entity.id,
+            username = entity.username,
+            name = entity.name,
+            email = entity.email,
+            phone = entity.phone,
+            introduction = entity.introduction,
+            nationalityId = entity.nationality?.id,
+            keywords = keywordMapStruct.toDtos(
+                entity.keywords
+                    .sortedWith(compareBy<Keyword> { it.priority }.thenBy { it.label }),
+            ),
+        )
+    }
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdBy", ignore = true)
@@ -17,5 +36,6 @@ interface UserMapStruct : GenericMapStruct<User, UserDto> {
     @Mapping(target = "updatedBy", ignore = true)
     @Mapping(target = "updatedDateTime", ignore = true)
     @Mapping(target = "nationality", ignore = true)
-    fun toEntity(request: CreateUserRequest): User
+    @Mapping(target = "keywords", ignore = true)
+    abstract fun toEntity(request: CreateUserRequest): User
 }
