c13825e632455e4a29f130385b81316a39ad90fb
ystc1247 <ystc1247@khu.ac.kr>
2026-03-25 16:14:48 +0900

    config: register optional request body resolver

--- src/main/kotlin/semo/backend/config/WebMvcConfig.kt
+++ src/main/kotlin/semo/backend/config/WebMvcConfig.kt
@@ -3,13 +3,16 @@ package semo.backend.config
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import semo.backend.config.argument.OptionalRequestBodyArgumentResolver
 import semo.backend.security.UserIdArgumentResolver
 
 @Configuration
 class WebMvcConfig(
+    private val optionalRequestBodyArgumentResolver: OptionalRequestBodyArgumentResolver,
     private val userIdArgumentResolver: UserIdArgumentResolver,
 ) : WebMvcConfigurer {
     override fun addArgumentResolvers(argumentResolvers: MutableList<HandlerMethodArgumentResolver>) {
+        argumentResolvers.add(optionalRequestBodyArgumentResolver)
         argumentResolvers.add(userIdArgumentResolver)
     }
 }
