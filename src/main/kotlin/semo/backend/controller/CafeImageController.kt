a073caaee22dd0e4460d67ef8186397a6141e9ac
ystc1247 <ystc1247@khu.ac.kr>
2026-03-26 10:33:17 +0900

    Update cafe image controller for url payloads

--- src/main/kotlin/semo/backend/controller/CafeImageController.kt
+++ src/main/kotlin/semo/backend/controller/CafeImageController.kt
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.multipart.MultipartFile
 import semo.backend.config.argument.OptionalRequestBody
+import semo.backend.controller.request.CreateCafeImageRequest
 import semo.backend.controller.request.UpdateCafeImageRequest
 import semo.backend.controller.response.CreateCafeImageResponse
 import semo.backend.controller.response.DeleteCafeImageResponse
@@ -48,11 +48,10 @@ class CafeImageController(
     @ResponseStatus(HttpStatus.CREATED)
     fun createCafeImage(
         @PathVariable cafeId: Long,
-        @RequestParam("file") file: MultipartFile,
-        @RequestParam("mainImage", required = false, defaultValue = "false") mainImage: Boolean,
+        @RequestBody request: CreateCafeImageRequest,
     ): CreateCafeImageResponse {
         return CreateCafeImageResponse(
-            image = cafeImageFacade.createCafeImage(cafeId, file, mainImage),
+            image = cafeImageFacade.createCafeImage(cafeId, request),
         )
     }
 
