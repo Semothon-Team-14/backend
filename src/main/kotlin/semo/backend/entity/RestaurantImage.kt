1b31106ac4582c212a231b8a29c7f9cdcc8a4261
ystc1247 <ystc1247@khu.ac.kr>
2026-03-26 10:30:55 +0900

    Add restaurant image entity

---
+++ src/main/kotlin/semo/backend/entity/RestaurantImage.kt
@@ -0,0 +1,32 @@
+package semo.backend.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "restaurant_images")
+class RestaurantImage(
+    @Column(name = "image_url", nullable = false)
+    var imageUrl: String,
+
+    @Column(name = "s3_key", nullable = false)
+    var s3Key: String,
+
+    @Column(name = "main_image", nullable = false)
+    var mainImage: Boolean,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    var restaurant: Restaurant,
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0
+}
