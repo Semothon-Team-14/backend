440d463e54b573783998dd216b65dd0426c0bf62
ystc1247 <ystc1247@khu.ac.kr>
2026-03-26 10:33:17 +0900

    Remove AWS S3 dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("software.amazon.awssdk:s3:2.39.2")
     implementation("org.apache.kafka:kafka-streams")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
