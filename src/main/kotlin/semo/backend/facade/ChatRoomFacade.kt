8d15e769bf191e0c9093d0842f032bcb7a32acd1
ystc1247 <ystc1247@khu.ac.kr>
2026-03-26 10:43:32 +0900

    Add chat room facade

---
+++ src/main/kotlin/semo/backend/facade/ChatRoomFacade.kt
@@ -0,0 +1,23 @@
+package semo.backend.facade
+
+import org.springframework.stereotype.Service
+import semo.backend.controller.request.InitializeChatRoomRequest
+import semo.backend.dto.ChatRoomDto
+import semo.backend.service.ChatRoomService
+
+@Service
+class ChatRoomFacade(
+    private val chatRoomService: ChatRoomService,
+) {
+    fun getChatRooms(userId: Long): List<ChatRoomDto> {
+        return chatRoomService.getChatRooms(userId)
+    }
+
+    fun getChatRoom(userId: Long, chatRoomId: Long): ChatRoomDto {
+        return chatRoomService.getChatRoom(userId, chatRoomId)
+    }
+
+    fun initializeChatRoom(userId: Long, request: InitializeChatRoomRequest): ChatRoomDto {
+        return chatRoomService.initializeChatRoom(userId, request)
+    }
+}
