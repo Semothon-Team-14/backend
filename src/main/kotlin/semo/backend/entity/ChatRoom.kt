44925ab9b05b52a7de2063a143dbfe0934966962
ystc1247 <ystc1247@khu.ac.kr>
2026-03-26 10:43:32 +0900

    Add chat room entity

---
+++ src/main/kotlin/semo/backend/entity/ChatRoom.kt
@@ -0,0 +1,34 @@
+package semo.backend.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "chat_rooms")
+class ChatRoom(
+    @Column(name = "name")
+    var name: String?,
+
+    @Column(name = "direct_chat", nullable = false)
+    var directChat: Boolean,
+
+    @Column(name = "created_date_time", nullable = false)
+    var createdDateTime: LocalDateTime,
+
+    @Column(name = "updated_date_time", nullable = false)
+    var updatedDateTime: LocalDateTime,
+
+    @OneToMany(mappedBy = "chatRoom", fetch = FetchType.LAZY)
+    var participants: MutableSet<ChatParticipant> = mutableSetOf(),
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0
+}
