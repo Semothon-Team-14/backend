540a1775df1075b01d40dde44b4f2de4fb877c60
ystc1247 <ystc1247@khu.ac.kr>
2026-03-26 10:30:55 +0900

    Add cafe dto

---
+++ src/main/kotlin/semo/backend/dto/CafeDto.kt
@@ -0,0 +1,14 @@
+package semo.backend.dto
+
+import java.math.BigDecimal
+
+data class CafeDto(
+    val id: Long,
+    val cityId: Long,
+    val name: String,
+    val phoneNumber: String?,
+    val address: String?,
+    val foodCategory: String?,
+    val latitude: BigDecimal?,
+    val longitude: BigDecimal?,
+)
