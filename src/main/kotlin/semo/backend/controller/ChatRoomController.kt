507bac098cb0cfed19e0154e2e03c401008aed4c
ystc1247 <ystc1247@khu.ac.kr>
2026-03-26 10:43:32 +0900

    Add chat room controller

---
+++ src/main/kotlin/semo/backend/controller/ChatRoomController.kt
@@ -0,0 +1,52 @@
+package semo.backend.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import semo.backend.controller.request.InitializeChatRoomRequest
+import semo.backend.controller.response.GetChatRoomResponse
+import semo.backend.controller.response.GetChatRoomsResponse
+import semo.backend.controller.response.InitializeChatRoomResponse
+import semo.backend.facade.ChatRoomFacade
+import semo.backend.security.UserId
+
+@RestController
+@RequestMapping("/chatrooms")
+class ChatRoomController(
+    private val chatRoomFacade: ChatRoomFacade,
+) {
+    @GetMapping
+    fun getChatRooms(
+        @UserId userId: Long,
+    ): GetChatRoomsResponse {
+        return GetChatRoomsResponse(
+            chatRooms = chatRoomFacade.getChatRooms(userId),
+        )
+    }
+
+    @GetMapping("/{chatRoomId}")
+    fun getChatRoom(
+        @UserId userId: Long,
+        @PathVariable chatRoomId: Long,
+    ): GetChatRoomResponse {
+        return GetChatRoomResponse(
+            chatRoom = chatRoomFacade.getChatRoom(userId, chatRoomId),
+        )
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun initializeChatRoom(
+        @UserId userId: Long,
+        @RequestBody request: InitializeChatRoomRequest,
+    ): InitializeChatRoomResponse {
+        return InitializeChatRoomResponse(
+            chatRoom = chatRoomFacade.initializeChatRoom(userId, request),
+        )
+    }
+}
