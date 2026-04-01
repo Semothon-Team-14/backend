96a48ff55f6c7a88501a9f9a23b00b3e2476cbe5
ystc1247 <ystc1247@khu.ac.kr>
2026-04-01 16:35:21 +0900

    Update src/main/kotlin/semo/backend/repository/jpa/MingleRepository.kt for mingles feature

---
+++ src/main/kotlin/semo/backend/repository/jpa/MingleRepository.kt
@@ -0,0 +1,8 @@
+package semo.backend.repository.jpa
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import semo.backend.entity.Mingle
+
+@Repository
+interface MingleRepository : JpaRepository<Mingle, Long>
