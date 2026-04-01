94b3e58afcd3a51e47be317748e3114395ef567f
ystc1247 <ystc1247@khu.ac.kr>
2026-04-01 17:32:49 +0900

    Set read-only transaction default in LocalService.kt

--- src/main/kotlin/semo/backend/service/LocalService.kt
+++ src/main/kotlin/semo/backend/service/LocalService.kt
@@ -20,6 +20,7 @@ import semo.backend.repository.jpa.UserRepository
 import semo.backend.util.applyIfProvided
 
 @Service
+@Transactional(readOnly = true)
 class LocalService(
     private val localRepository: LocalRepository,
     private val userRepository: UserRepository,
