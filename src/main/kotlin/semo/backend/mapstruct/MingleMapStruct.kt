f18b85cb5818323bc1628cfe5dd3289b1dbab41e
ystc1247 <ystc1247@khu.ac.kr>
2026-04-01 16:35:21 +0900

    Update src/main/kotlin/semo/backend/mapstruct/MingleMapStruct.kt for mingles feature

---
+++ src/main/kotlin/semo/backend/mapstruct/MingleMapStruct.kt
@@ -0,0 +1,25 @@
+package semo.backend.mapstruct
+
+import org.springframework.stereotype.Component
+import semo.backend.dto.MingleDto
+import semo.backend.entity.Mingle
+
+@Component
+class MingleMapStruct(
+    private val cityMapStruct: CityMapStruct,
+) {
+    fun toDto(entity: Mingle): MingleDto {
+        return MingleDto(
+            id = entity.id,
+            city = cityMapStruct.toDto(entity.city),
+            title = entity.title,
+            description = entity.description,
+            createdDateTime = entity.createdDateTime,
+            updatedDateTime = entity.updatedDateTime,
+        )
+    }
+
+    fun toDtos(entities: List<Mingle>): List<MingleDto> {
+        return entities.map(::toDto)
+    }
+}
