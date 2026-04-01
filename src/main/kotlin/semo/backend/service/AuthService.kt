5c04e83a825116f9dc4e2fa6c48148e76b10346c
ystc1247 <ystc1247@khu.ac.kr>
2026-04-01 17:32:49 +0900

    Set read-only transaction default in AuthService.kt

--- src/main/kotlin/semo/backend/service/AuthService.kt
+++ src/main/kotlin/semo/backend/service/AuthService.kt
@@ -1,12 +1,14 @@
 package semo.backend.service
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import semo.backend.controller.request.LoginRequest
 import semo.backend.exception.auth.InvalidLoginException
 import semo.backend.repository.jpa.UserRepository
 import semo.backend.security.AccessTokenService
 
 @Service
+@Transactional(readOnly = true)
 class AuthService(
     private val userRepository: UserRepository,
     private val accessTokenService: AccessTokenService,
