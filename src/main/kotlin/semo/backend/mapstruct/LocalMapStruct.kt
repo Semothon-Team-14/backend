3e984042105f34379b50473ae4ae1d57e7f673c2
ystc1247 <ystc1247@khu.ac.kr>
2026-04-01 17:23:50 +0900

    Update src/main/kotlin/semo/backend/mapstruct/LocalMapStruct.kt for locals and quick-match targeting

---
+++ src/main/kotlin/semo/backend/mapstruct/LocalMapStruct.kt
@@ -0,0 +1,24 @@
+package semo.backend.mapstruct
+
+import org.springframework.stereotype.Component
+import semo.backend.dto.LocalDto
+import semo.backend.entity.Local
+
+@Component
+class LocalMapStruct(
+    private val cityMapStruct: CityMapStruct,
+) {
+    fun toDto(entity: Local): LocalDto {
+        return LocalDto(
+            id = entity.id,
+            userId = entity.user.id,
+            city = cityMapStruct.toDto(entity.city),
+            createdDateTime = entity.createdDateTime,
+            updatedDateTime = entity.updatedDateTime,
+        )
+    }
+
+    fun toDtos(entities: List<Local>): List<LocalDto> {
+        return entities.map(::toDto)
+    }
+}
