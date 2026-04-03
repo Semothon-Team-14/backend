56f63e26cd1c2e044e9e776aa45c481b27eabd68
ystc1247 <ystc1247@khu.ac.kr>
2026-04-03 23:33:02 +0900

    Allow place and meet datetime in create mingle request

--- src/main/kotlin/semo/backend/controller/request/CreateMingleRequest.kt
+++ src/main/kotlin/semo/backend/controller/request/CreateMingleRequest.kt
@@ -1,11 +1,14 @@
 package semo.backend.controller.request
 
 import java.math.BigDecimal
+import java.time.LocalDateTime
 
 data class CreateMingleRequest(
     val cityId: Long,
     val title: String,
     val description: String? = null,
+    val placeName: String? = null,
+    val meetDateTime: LocalDateTime? = null,
     val latitude: BigDecimal? = null,
     val longitude: BigDecimal? = null,
 )
