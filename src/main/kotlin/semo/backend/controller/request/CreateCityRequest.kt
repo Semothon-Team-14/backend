e5a32590c2ac02deee7c4646b5d4fdcd926d75fe
ystc1247 <ystc1247@khu.ac.kr>
2026-04-03 23:52:40 +0900

    Allow city center coordinates in create city request

--- src/main/kotlin/semo/backend/controller/request/CreateCityRequest.kt
+++ src/main/kotlin/semo/backend/controller/request/CreateCityRequest.kt
@@ -1,7 +1,11 @@
 package semo.backend.controller.request
 
+import java.math.BigDecimal
+
 data class CreateCityRequest(
     val cityNameEnglish: String,
     val cityNameKorean: String,
     val representativeImageUrl: String? = null,
+    val centerLatitude: BigDecimal? = null,
+    val centerLongitude: BigDecimal? = null,
 )
