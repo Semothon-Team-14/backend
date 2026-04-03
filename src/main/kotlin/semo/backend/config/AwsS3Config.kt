6238e994cbf2a18f814a81413c082e6e464c90da
ystc1247 <ystc1247@khu.ac.kr>
2026-04-03 21:48:48 +0900

    Allow S3 client init with profile pictures bucket configuration

--- src/main/kotlin/semo/backend/config/AwsS3Config.kt
+++ src/main/kotlin/semo/backend/config/AwsS3Config.kt
@@ -14,13 +14,16 @@ import software.amazon.awssdk.services.s3.S3Client
 class AwsS3Config {
     @Bean
     @ConditionalOnExpression(
-        "'\${app.aws.access-key-id:}' != '' && '\${app.aws.secret-access-key:}' != '' && '\${app.aws.region:}' != '' && '\${app.aws.s3-bucket:}' != ''",
+        "'\${app.aws.access-key-id:}' != '' && '\${app.aws.secret-access-key:}' != '' && '\${app.aws.region:}' != '' && ('\${app.aws.s3-bucket:}' != '' || '\${app.aws.profile-pictures-bucket:}' != '')",
     )
     fun s3Client(awsS3Properties: AwsS3Properties): S3Client {
         requireNoUnresolvedPlaceholder(awsS3Properties.accessKeyId, "AWS_ACCESS_KEY_ID")
         requireNoUnresolvedPlaceholder(awsS3Properties.secretAccessKey, "AWS_SECRET_ACCESS_KEY")
         requireNoUnresolvedPlaceholder(awsS3Properties.region, "AWS_REGION")
-        requireNoUnresolvedPlaceholder(awsS3Properties.s3Bucket, "AWS_S3_BUCKET")
+        if (awsS3Properties.s3Bucket.isNotBlank()) {
+            requireNoUnresolvedPlaceholder(awsS3Properties.s3Bucket, "AWS_S3_BUCKET")
+        }
+        requireNoUnresolvedPlaceholder(awsS3Properties.profilePicturesBucket, "AWS_PROFILE_PICTURES_BUCKET")
 
         return S3Client.builder()
             .region(Region.of(awsS3Properties.region))
