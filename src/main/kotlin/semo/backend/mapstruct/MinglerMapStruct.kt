a60bb1ee5262ba373fd6d6c07a52c4a0164afa62
ystc1247 <ystc1247@khu.ac.kr>
2026-04-04 06:22:05 +0900

    Populate mingle participant local flag from locals

--- src/main/kotlin/semo/backend/mapstruct/MinglerMapStruct.kt
+++ src/main/kotlin/semo/backend/mapstruct/MinglerMapStruct.kt
@@ -3,15 +3,21 @@ package semo.backend.mapstruct
 import org.springframework.stereotype.Component
 import semo.backend.dto.MinglerDto
 import semo.backend.entity.Mingler
+import semo.backend.repository.jpa.LocalRepository
 
 @Component
 class MinglerMapStruct(
     private val mingleMapStruct: MingleMapStruct,
+    private val localRepository: LocalRepository,
 ) {
     fun toDto(entity: Mingler): MinglerDto {
+        val mingleCityId = entity.mingle.city.id
+        val minglerUserId = entity.user.id
+
         return MinglerDto(
             id = entity.id,
             userId = entity.user.id,
+            local = localRepository.existsByUserIdAndCityId(minglerUserId, mingleCityId),
             mingle = mingleMapStruct.toDto(entity.mingle),
             createdDateTime = entity.createdDateTime,
             updatedDateTime = entity.updatedDateTime,
