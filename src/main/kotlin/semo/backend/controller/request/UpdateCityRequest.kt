6690231ceb31e82d9184219ef9757028d716ddf0
ystc1247 <ystc1247@khu.ac.kr>
2026-04-03 22:40:27 +0900

    Allow representative image url on city update request

--- src/main/kotlin/semo/backend/controller/request/UpdateCityRequest.kt
+++ src/main/kotlin/semo/backend/controller/request/UpdateCityRequest.kt
@@ -5,4 +5,5 @@ import java.util.Optional
 data class UpdateCityRequest(
     val cityNameEnglish: Optional<String>? = null,
     val cityNameKorean: Optional<String>? = null,
+    val representativeImageUrl: Optional<String>? = null,
 )
