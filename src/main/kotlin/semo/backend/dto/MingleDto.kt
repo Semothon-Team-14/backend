c3536943839e80f0a42cd2edc1a5c3a2a4cfe080
ystc1247 <ystc1247@khu.ac.kr>
2026-04-03 17:01:09 +0900

    Expose mingle coordinates in dto

--- src/main/kotlin/semo/backend/dto/MingleDto.kt
+++ src/main/kotlin/semo/backend/dto/MingleDto.kt
@@ -1,12 +1,15 @@
 package semo.backend.dto
 
 import java.time.LocalDateTime
+import java.math.BigDecimal
 
 data class MingleDto(
     val id: Long,
     val city: CityDto,
     val title: String,
     val description: String?,
+    val latitude: BigDecimal?,
+    val longitude: BigDecimal?,
     val createdDateTime: LocalDateTime,
     val updatedDateTime: LocalDateTime,
 )
