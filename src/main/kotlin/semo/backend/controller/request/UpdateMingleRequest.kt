9287ac84edf212ed3697c77a49cf8d4ac6a6fa3b
ystc1247 <ystc1247@khu.ac.kr>
2026-04-03 17:01:09 +0900

    Accept mingle coordinates in update request

--- src/main/kotlin/semo/backend/controller/request/UpdateMingleRequest.kt
+++ src/main/kotlin/semo/backend/controller/request/UpdateMingleRequest.kt
@@ -1,9 +1,12 @@
 package semo.backend.controller.request
 
+import java.math.BigDecimal
 import java.util.Optional
 
 data class UpdateMingleRequest(
     val cityId: Optional<Long>? = null,
     val title: Optional<String>? = null,
     val description: Optional<String>? = null,
+    val latitude: Optional<BigDecimal>? = null,
+    val longitude: Optional<BigDecimal>? = null,
 )
