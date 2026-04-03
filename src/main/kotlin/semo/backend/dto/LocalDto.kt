6b0955f07551427e9647bbd4cc67f01b1e0f3293
ystc1247 <ystc1247@khu.ac.kr>
2026-04-04 03:26:33 +0900

    Expose local available time text in local dto

--- src/main/kotlin/semo/backend/dto/LocalDto.kt
+++ src/main/kotlin/semo/backend/dto/LocalDto.kt
@@ -6,6 +6,7 @@ data class LocalDto(
     val id: Long,
     val userId: Long,
     val city: CityDto,
+    val availableTimeText: String?,
     val createdDateTime: LocalDateTime,
     val updatedDateTime: LocalDateTime,
 )
