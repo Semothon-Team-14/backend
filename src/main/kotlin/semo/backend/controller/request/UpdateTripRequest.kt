35c7039755f2947d17f5309b827af6e6e3d38355
ystc1247 <ystc1247@khu.ac.kr>
2026-04-04 02:34:25 +0900

    Accept departure datetimes in update trip request

--- src/main/kotlin/semo/backend/controller/request/UpdateTripRequest.kt
+++ src/main/kotlin/semo/backend/controller/request/UpdateTripRequest.kt
@@ -1,12 +1,15 @@
 package semo.backend.controller.request
 
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.Optional
 
 data class UpdateTripRequest(
     val title: Optional<String>? = null,
     val startDate: Optional<LocalDate>? = null,
     val endDate: Optional<LocalDate>? = null,
+    val departureDateTime: Optional<LocalDateTime>? = null,
+    val departureLandingDateTime: Optional<LocalDateTime>? = null,
     val userId: Optional<Long>? = null,
     val cityId: Optional<Long>? = null,
 )
