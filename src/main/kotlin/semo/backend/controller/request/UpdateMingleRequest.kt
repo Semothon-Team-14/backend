21f76b600099572dcfcdc676309d50e8164cb9e3
ystc1247 <ystc1247@khu.ac.kr>
2026-04-03 23:33:03 +0900

    Allow place and meet datetime in update mingle request

--- src/main/kotlin/semo/backend/controller/request/UpdateMingleRequest.kt
+++ src/main/kotlin/semo/backend/controller/request/UpdateMingleRequest.kt
@@ -1,12 +1,15 @@
 package semo.backend.controller.request
 
 import java.math.BigDecimal
+import java.time.LocalDateTime
 import java.util.Optional
 
 data class UpdateMingleRequest(
     val cityId: Optional<Long>? = null,
     val title: Optional<String>? = null,
     val description: Optional<String>? = null,
+    val placeName: Optional<String>? = null,
+    val meetDateTime: Optional<LocalDateTime>? = null,
     val latitude: Optional<BigDecimal>? = null,
     val longitude: Optional<BigDecimal>? = null,
 )
