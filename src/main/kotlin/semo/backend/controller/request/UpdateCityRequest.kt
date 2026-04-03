c8e7fea17d38907fa30413eb5e6b5bf1469ddefc
ystc1247 <ystc1247@khu.ac.kr>
2026-04-03 23:52:42 +0900

    Allow city center coordinates in update city request

--- src/main/kotlin/semo/backend/controller/request/UpdateCityRequest.kt
+++ src/main/kotlin/semo/backend/controller/request/UpdateCityRequest.kt
@@ -1,9 +1,12 @@
 package semo.backend.controller.request
 
+import java.math.BigDecimal
 import java.util.Optional
 
 data class UpdateCityRequest(
     val cityNameEnglish: Optional<String>? = null,
     val cityNameKorean: Optional<String>? = null,
     val representativeImageUrl: Optional<String>? = null,
+    val centerLatitude: Optional<BigDecimal>? = null,
+    val centerLongitude: Optional<BigDecimal>? = null,
 )
